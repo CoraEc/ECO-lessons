378877da1c04ba67f29c5d7410a4fad997bd72e9
CoraEc <oleg.v.kobets@gmail.com>
2015-04-16 23:02:33 +0300

    Task2 ECOBeingUpdate -> begining

---
+++ ECOObjC/Task2 ECOBeingUpdate/ECOBeing.m
@@ -0,0 +1,13 @@
+//
+//  ECOBeing.m
+//  ECO lessons
+//
+//  Created by Oleg Kobets on 16.04.15.
+//  Copyright (c) 2015 Oleg Kobets. All rights reserved.
+//
+
+#import "ECOBeing.h"
+
+@implementation ECOBeing
+
+@end
